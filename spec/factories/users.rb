e54442ad38cc44e9bf460414bcd50340fe4e272c
Shorojit_Sarkar <shorojit.kumar@welldev.io>
2024-01-03 17:54:33 +0600

    fix email case insensitive problem

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    email { Faker::Internet.unique.email}
+    email { Faker::Internet.unique.email.downcase}
     phone {"+88013#{Faker::PhoneNumber.unique.subscriber_number(length: 8)}"}
     address { Faker::Address.full_address  }
     role { 0 }

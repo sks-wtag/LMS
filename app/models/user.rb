579b874bb282ebb7f524d55a1c4eb340b5a02044
Shorojit_Sarkar <shorojit.kumar@welldev.io>
2024-01-06 18:33:51 +0600

    add login features

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # before_validation :set_default_country_code
+  has_secure_password
+  CONFIRMATION_TOKEN_EXPIRATION = 10.minutes
+  MAILER_FROM_EMAIL = ENV['MAILER_FROM_EMAIL']
+  before_save :downcase_email
   has_many :enrollments
   has_many :courses, through: :enrollments
   has_many :user_course_progresses
@@ -30,5 +33,31 @@ class User < ApplicationRecord
   def name
     "#{first_name} #{last_name}"
   end
+
+  def confirm!
+    update_columns(confirmed_at: Time.current)
+  end
+
+  def confirmed?
+    confirmed_at.present?
+  end
+
+  def generate_confirmation_token
+    signed_id expires_in: CONFIRMATION_TOKEN_EXPIRATION, purpose: :confirm_email
+  end
+
+  def unconfirmed?
+    !confirmed?
+  end
   
+  def send_confirmation_email!
+    confirmation_token = generate_confirmation_token
+    UserMailer.confirmation(self, confirmation_token).deliver_now
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  post 'sign_up', to: 'users#create'
+  get 'sign_up', to: 'users#new'
+  
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get 'login', to: 'sessions#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
+  resources :confirmations, only: %i[create edit new], param: :confirmation_token
 
   # Defines the root path route ("/")
   # root "posts#index"

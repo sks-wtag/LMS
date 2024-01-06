198f6ad120d35970170aac8d497bd3cedbc41a8c
Shorojit_Sarkar <shorojit.kumar@welldev.io>
2024-01-07 02:09:25 +0600

    add signup features

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,19 @@
 class UserMailer < ApplicationMailer
-
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_mailer.confirmation.subject
-  
+
   default from: User::MAILER_FROM_EMAIL
-  def confirmation(user,_confirmation_token)
-    @user=user
-    @confirmation_token=_confirmation_token
-    mail to:@user.email, subject: 'Confirmation instructions'
+  def confirmation(user, confirmation_token)
+    @user = user
+    @confirmation_token = confirmation_token
+    mail to: @user.email, subject: 'Confirmation instructions'
+  end
+
+  def password_reset(user, password_reset_token)
+    @password_reset_token = password_reset_token
+    @user = user
+    mail to: @user.email, subject: 'Password reset instruction'
   end
 end

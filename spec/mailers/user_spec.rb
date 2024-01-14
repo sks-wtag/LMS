6d68f309b6bfd594a2739e0b2b06bd00f6132cc9
Shorojit Kumar Sarkar <shorojit.kumar@welldev.io>
2024-01-14 12:58:45 +0600

    add some user test case

--- spec/mailers/user_spec.rb
+++ spec/mailers/user_spec.rb
@@ -1,17 +1,21 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
+  let!(:user) {create(:user) }
   describe "confirmation" do
-    let(:mail) { UserMailer.confirmation }
-
+    let(:mail) { UserMailer.confirmation(user, user.generate_confirmation_token) }
     it "renders the headers" do
-      expect(mail.subject).to eq("Confirmation")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.subject).to eq("Confirmation instructions")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq([ENV['ADMINSTRATOR_EMAIL']])
     end
-
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+  end
+  describe "Password_Reset" do
+    let(:mail) { UserMailer.password_reset(user, user.generate_confirmation_token) }
+    it "renders the headers" do
+      expect(mail.subject).to eq("Password reset instruction")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq([ENV['ADMINSTRATOR_EMAIL']])
     end
   end
 

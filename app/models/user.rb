524cd3635070b064ad30aa94a71bf3f731a8603c
Shorojit Kumar Sarkar <shorojit.kumar@welldev.io>
2024-01-15 15:26:24 +0600

    fix: comment remove

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,12 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # before_validation :set_default_country_code
   has_many :enrollments
   has_many :courses, through: :enrollments
   has_many :user_course_progresses
-  # first_name and last_name can't be nul;
+  
   validates :first_name, :last_name, :email, :phone, :address, presence: true
-  # first_name ans last_name must have at least 2-20 character
   validates :first_name, :last_name, length: { minimum: 2, maximum: 20 }
-
-  # first name and lastname must start with capital letter and match this regular expression
   validates :first_name, :last_name, format: { with: /\A[A-Z]+[a-z]*\z/ }
   # email must be unique and case insensitive
   validates :email, uniqueness: { case_sensitive: false }

7ad2d092e1c5a94f6ca3bdd55bd1f4e71c04e5e2
Shorojit Kumar Sarkar <shorojit.kumar@welldev.io>
2024-01-15 15:27:37 +0600

    fix: remove all comment in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,21 +8,16 @@ class User < ApplicationRecord
   validates :first_name, :last_name, :email, :phone, :address, presence: true
   validates :first_name, :last_name, length: { minimum: 2, maximum: 20 }
   validates :first_name, :last_name, format: { with: /\A[A-Z]+[a-z]*\z/ }
-  # email must be unique and case insensitive
   validates :email, uniqueness: { case_sensitive: false }
-  # email should be valid according to this regular expression
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates_plausible_phone :phone, presence: true
-  # address have at least 10 character and maximum 100 character
   validates :address, length: { minimum: 10, maximum: 100 }
-  # set default country code for bangladesh
   phony_normalize :phone, default_country_code: 'BD'
   enum role: {
     learner: 0,
     instructor: 1,
     admin: 2
   }
-  # this function name return the full_name of a person
   def name
     "#{first_name} #{last_name}"
   end

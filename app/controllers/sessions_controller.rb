3c206dfe814e0d4680c9e42c6fee1851d79c00c3
Shorojit Kumar Sarkar <152980765+sks-wtag@users.noreply.github.com>
2024-01-23 16:25:56 +0600

    fix error of session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,13 @@
 class SessionsController < ApplicationController
   before_action :redirect_if_authenticated, only: %i[create new]
   def create
-    @user = User.find_by(email: params[:user][:email].downcase)
+    @user = User.find_by(email: params[:user][:email]&.downcase)
     if @user.present?
       if @user.unconfirmed?
         redirect_to new_confirmation_path, notice: 'Incorrect credentials'
       elsif @user.authenticate(params[:user][:password])
         login(@user)
-        redirect_to root_path, notice: 'Successfully log in'
+        redirect_to root_path, notice: 'Successfully logged in'
       else
         flash[:notice] = 'Invalid credentials'
         render 'sessions/new', status: :unprocessable_entity
@@ -22,7 +22,7 @@ class SessionsController < ApplicationController
 
   def destroy
     logout
-    redirect_to root_path, notice: 'Signed out'
+    redirect_to root_path, notice: 'Logged out'
   end
   def new; end
 end

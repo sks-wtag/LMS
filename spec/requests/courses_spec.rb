80f303afd2fdd4d7cd8b87cb945d38350b57bd88
Shorojit Kumar Sarkar <152980765+sks-wtag@users.noreply.github.com>
2024-02-04 19:03:22 +0600

    add test case in course model

--- spec/requests/courses_spec.rb
+++ spec/requests/courses_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Courses", type: :request do
-  describe "GET /index" do
+  let!(:user) { create(:user,role: "admin") }
+  before do
+    login(user)
+  end
+  describe "GET /dashboard/add_course" do
 
+    it 'when it created a valid request' do
+      login(user)
+      get dashboard_add_course_path
+      expect(response).to render_template(:new_course)
+    end
+
+  end
+  def login(user)
+    post login_path, params: { user: { email: user.email, password: user.password}}
   end
 end
